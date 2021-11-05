cccfe8b7b0707a143d8fba72c1ada83dcee51e98
Paulo Baptista <paulofbaptista@gmail.com>
2021-11-05 10:07:18 +0000

    Layout changes and theme change

--- src/routes/[searchValue]/__layout.svelte
+++ src/routes/[searchValue]/__layout.svelte
@@ -6,28 +6,4 @@
 	};
 </script>
 
-<nav class="container mx-auto">
-	<div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
-		<div class="flex-none">
-			<button class="btn btn-square btn-ghost" on:click={goBack}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					class="inline-block w-6 h-6 stroke-current text-success"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M10 19l-7-7m0 0l7-7m-7 7h18"
-					/>
-				</svg>
-			</button>
-		</div>
-		<div class="flex-1 px-2 mx-2">
-			<span class="text-lg font-bold"> Back </span>
-		</div>
-	</div>
-</nav>
 <slot></slot>
